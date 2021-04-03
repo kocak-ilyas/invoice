5c3de8cc00afb1ea7f7534ae85fdb6274ad0a307
ilyas kocak <kocakilyas18@gmail.com>
2021-04-04 00:39:09 +0300

    reactScript

--- app/app.js
+++ app/app.js
@@ -1,20 +1,9 @@
-import React from "react";
-// import { Route, Switch } from "react-router-dom";
-// import "../../styles/styles.css";
+import React from 'react';
 
-export default function App() { 
-  return (
-    <div className="container-fluid mt-4">
-<h3> okkk</h3>
-
-      {/* <Switch>
-        <Route exact path="/" component={Dashboard} />
-        <Route exact path="/cart" component={CartPage} />
-        <Route path="/signIn" component={SignIn} />
-        <Route path="/creditcard" component={CreditCard} />
-        <Route path="/createAcount" component={CreateAcount} />
-        <Route component={NotFound} />
-      </Switch> */}
-    </div>
-  );
+export default function App() {
+	return (
+		<div className="container-fluid mt-4">
+			<h3> okkk</h3>
+		</div>
+	);
 }
--- app/index.js
+++ app/index.js
@@ -1,21 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { Provider } from "react-redux";
-// import { BrowserRouter } from "react-router-dom";
-// import "./styles/styles.css"
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import "alertifyjs/build/css/alertify.min.css";
 import App from './App.js';
-// import configureStore from "./redux/reducers/configureStore";
-// const store = configureStore();
 
 ReactDOM.render(
 	<React.StrictMode>
-		{/* <BrowserRouter>
-      <Provider store={store}> */}
 		<App />
-		{/* </Provider>
-    </BrowserRouter> */}
 	</React.StrictMode>,
 	document.getElementById('root')
 );
