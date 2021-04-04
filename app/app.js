9c505c91bb4bf790243f87c7029b4f77afbed16b
ilyas kocak <kocakilyas18@gmail.com>
2021-04-04 18:59:44 +0300

    bootstrap added

--- app/app.js
+++ app/app.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
+// import "../node_modules/bootstrap/dist/css/bootstrap-grid.min.css";
 
 function App() {
-	return <h3> okkk</h3>;
+	return(
+	<div>
+		{/* <!-- Button trigger modal --> */}
+<button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Launch demo modal
+</button>
+
+{/* <!-- Modal --> */}
+<div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div className="modal-dialog">
+    <div className="modal-content">
+      <div className="modal-header">
+        <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div className="modal-body">
+        ...
+      </div>
+      <div className="modal-footer">
+        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" className="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+	</div>
+	)
+
 }
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('app'));
 
 if (module.hot) {
 	module.hot.accept();
